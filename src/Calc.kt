7a0a9d08362d51e63127f8f68d6688d1c69f2927
BlakeAvery <simmons.jared76@gmail.com>
2018-03-06 11:17:58 -0500

    A few variable changes
    For more cohesion from vals in Main.kt

--- src/Calc.kt
+++ src/Calc.kt
@@ -2,10 +2,10 @@ class Calc {
     fun isGrowth(wan: Double, tsuu: Double): Boolean {
         return (wan / tsuu) >= 1.0
     }
-    fun above(wan: Double): String {
-        return if(wan > 0.0) "above" else "below"
+    fun above(num: Double): String {
+        return if(num > 0.0) "above" else "below"
     }
-    fun yint(wan: Double, tsuu: Double, intercept: Double, expo: Double): Double {
+    fun yint(num: Double, wan: Double, tsuu: Double, intercept: Double, expo: Double): Double {
         //TODO: Implement!
         return 0.0
     }
